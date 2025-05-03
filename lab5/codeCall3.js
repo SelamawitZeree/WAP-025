ec38d8c4ae35ee82140fd5348d026117b8f93d76
SelamawitZeree <selamawitzere7@gmail.com>
2025-05-02 23:42:12 -0500

    code

--- lab5/codeCall3.js
+++ lab5/codeCall3.js
@@ -1,13 +1,21 @@
-//Question 3: Fix the code below using wrapper, bind, call, or apply.
+'use strict';
+function askPassword(ok, fail) {
+    let password =prompt('Password?', '');
+    if (password == 'rockstar') ok();
+    else fail();
+}
 
-let group = {
-    title: "Our Group",
-    students: ["John", "Pete", "Alice"],
-    showList: function () {
-        this.students.forEach(function (student) {
-            console.log(this.title + ": " + student
-            );
-        });
-    }
+let user = {
+    name: 'John',
+
+    loginOk() {
+        alert(`${this.name} logged in`);
+    },
+
+    loginFail() {
+        alert(`${this.name} failed to log in`);
+    },
 };
-group.showList();
+
+// askPassword(user.loginOk, user.loginFail)
+askPassword(user.loginOk.bind(user), user.loginFail.bind(user));
